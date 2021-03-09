59e82008a10514d95614073f4da59ecc13318d0b
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-03-09 23:40:18 +0600

    added staticfile,setup the template and added views files of blog page above all set the statics files

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 import datetime
 # Create your views here.
 def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    # now = datetime.datetime.now()
+    # html = "<html><body>It is now %s.</body></html>" % now
+    return render(request, 'blog/blog.html')
