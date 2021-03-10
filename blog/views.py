f2e2a898cfde989efabe6659cbdbbb5490dc0da5
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-03-10 19:23:41 +0600

    Added daynamic data in single blog plage ,ande update view

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
+from .models import Post,Category,Comment
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
-def current_datetime(request):
-    # now = datetime.datetime.now()
-    # html = "<html><body>It is now %s.</body></html>" % now
-    return render(request, 'blog/blog.html')
+def blog(request):
+    blog = Post.objects.order_by('-timestamp')
+    categories = Category.objects.filter()
+    paginator = Paginator(blog, 2)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    # category_count = get_category_count()
+
+    context = {
+        'listings':paged_listings,
+        'categories':categories,
+        
+    }
+    return render(request, 'blog/blog.html',context)
