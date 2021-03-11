40c0f202b7dd4987f4e4adf9a7ef915d42281bb3
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-03-11 15:10:55 +0600

    update login template

--- user/forms.py
+++ user/forms.py
@@ -4,10 +4,62 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=30,label= 'User Name :')
-    email = forms.EmailField(max_length=200,label= 'Email :')
+    # username = forms.CharField(max_length=30,label= 'User Name :')
+    # email = forms.EmailField(max_length=200,label= 'Email :')
 
-
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        self.fields['username'].label = "User Name"
+        self.fields['email'].label = "Email"
+        self.fields['password1'].label = "Password"
+        self.fields['password2'].label = "Confirm Password"
+        for fieldname in ['password1', 'password2']:
+            self.fields[fieldname].help_text = None
+        self.fields['username'].widget.attrs.update(
+            {
+                'placeholder': 'Enter User Name',
+            }
+        )
+        self.fields['email'].widget.attrs.update(
+            {
+                'placeholder': 'Email',
+            }
+        ) 
+        self.fields['password1'].widget.attrs.update(
+            {
+                'placeholder': 'Enter Password',
+            }
+        )
+        self.fields['password2'].widget.attrs.update(
+            {
+                'placeholder': 'Confirm Password',
+            }
+        )
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
+
+
+class DoctorProfileUpdateForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(DoctorProfileUpdateForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs.update(
+            {
+                'placeholder': 'Enter First Name',
+            }
+        )
+        self.fields['last_name'].widget.attrs.update(
+            {
+                'placeholder': 'Enter Last Name',
+            }
+        )
+        self.fields['email'].widget.attrs.update(
+            {
+                'placeholder': 'Email',
+            }
+        )
+
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email"]        
