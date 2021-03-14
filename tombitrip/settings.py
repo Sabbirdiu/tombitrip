e8cb30678808bf3eb11a1bdc4b3b260bdc316701
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-03-14 11:33:00 +0600

    remove static

--- tombitrip/settings.py
+++ tombitrip/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,6 +139,3 @@ AUTHENTICATION_BACKENDS = [
 ]
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
