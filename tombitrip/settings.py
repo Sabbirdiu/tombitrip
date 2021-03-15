abdb96fd669ac6bea9429a21f403048aad053a5a
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-03-15 11:50:45 +0600

    added ckeditor

--- tombitrip/settings.py
+++ tombitrip/settings.py
@@ -141,3 +141,18 @@ AUTHENTICATION_BACKENDS = [
 ]
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
+CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
+CKEDITOR_IMAGE_BACKEND = "pillow"
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': None,
+    },
+}
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
