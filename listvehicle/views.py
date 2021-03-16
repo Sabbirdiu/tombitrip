87a59c8ae4d118ea21d0ae71fa6ade15cf8fa8cf
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-03-16 22:32:57 +0600

    added videos

--- listvehicle/views.py
+++ listvehicle/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from .models import Videos
 # Create your views here.
 def listvehicle(request):
-    return render(request,'listvehicle/listvehicle.html')
+    videos = Videos.objects.all()
+    context ={
+        'videos':videos,
+    }
+    return render(request,'listvehicle/listvehicle.html',context)
+
+  
