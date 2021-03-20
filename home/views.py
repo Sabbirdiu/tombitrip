fffa9544e4a4dda8b57a9f91e08d4ab1160c6866
Sabbirdiu <sabbir.s.dk7@gmail.com>
2021-03-20 10:06:58 +0600

    Added about and agents in experience details page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Experience
+from listvehicle.models import About,Agents
 # Create your views here.
 def index(request):
     exp = Experience.objects.all()
@@ -9,9 +10,12 @@ def index(request):
     return render(request,'home/home.html',context)
 def exp_details(request,slug):
     exp = get_object_or_404(Experience,slug=slug) 
-
+    about = About.objects.get()
+    agents = Agents.objects.all()  
     context = {
-        'exp':exp
+        'exp':exp,
+        'about':about,
+        'agents':agents,
     }
     return render(request,'home/details.html',context)
         
